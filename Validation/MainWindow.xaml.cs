75f5155b84066588862ba87fa9dc1295e8c16191
KlaasHagge <klaash@ppedv.de>
2022-01-26 12:04:57 +0100

    Validation

---
+++ Validation/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Validation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show((this.DataContext as Person).Alter.ToString());
+        }
+    }
+
+    public class ValidationRuleCollection : Collection<ValidationRule> { }
+
+    public class ValidationMarkup : MarkupExtension
+    {
+        private Binding binding;
+
+        public ValidationMarkup(Binding binding, ValidationRuleCollection validationRules)
+        {
+            this.binding = binding;
+
+            foreach (var item in validationRules)
+            {
+                binding.ValidationRules.Add(item);
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+}
---
+++ Validation/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    internal class Person : IDataErrorInfo
+    {
+        private string name;
+        public string Name 
+        { 
+            get => name;
+            set 
+            {
+                if (value.All(x => Char.IsLetter(x)))
+                    name = value;
+                else
+                    throw new Exception("Bitte gib nur Buchstaben ein");
+
+            }
+        }
+
+        public int Alter { get; set; }
+
+        public string Error => null;
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case nameof(Alter):
+                        if (Alter < 0 || Alter > 100) return "Bitte gib dein wahres Alter ein";
+
+                        break;
+                    default:
+                        break;
+                }
+
+                return null;
+            }
+        }
+    }
+}
