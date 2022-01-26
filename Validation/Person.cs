362430915de0cbbed87fd30a53d2c731e9985644
KlaasHagge <klaash@ppedv.de>
2022-01-26 14:53:04 +0100

    Trigger (kommentiert)

--- Validation/Person.cs
+++ Validation/Person.cs
@@ -7,35 +7,48 @@ using System.Threading.Tasks;
 
 namespace Validation
 {
-    internal class Person : IDataErrorInfo
+    //Für ValidatesOnDataErrors muss z.B. das Interface IDataErrorInfo implementiert werden. Dieses erfordert die Einbindung von zwei zusätzlichen
+    public class Person : IDataErrorInfo, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         private string name;
-        public string Name 
-        { 
-            get => name;
-            set 
+        public string Name
+        {
+            get { return name; }
+            set
             {
+                //Bei ValidatesOnException wird im Fehlerfall eine Exception geworfen, welche von der GUI aufgefangen
+                //wird und als Validierungsfehler interpretiert wird. Die Exception-Message ist der
+                //ErrorContent
                 if (value.All(x => Char.IsLetter(x)))
                     name = value;
                 else
-                    throw new Exception("Bitte gib nur Buchstaben ein");
-
+                    throw new Exception("Bitte gib nur Buchstaben ein.");
             }
         }
 
         public int Alter { get; set; }
 
+        private string? iP;
+        public string? IP { get => iP; set { iP = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IP))); } }
+
+
+        //Von IDataErrorInfo geforderte Properties
+
+        //Error wird von WPF nicht benutzt
         public string Error => null;
 
+        //Diese Property wird von der GUI als Validierung verwendet. Wenn ein nicht-leerer String zurückgegeben wird, 
+        //dann wird dies als Fehler interpretiert und dieser String als Fehlermeldung
         public string this[string columnName]
         {
             get
             {
                 switch (columnName)
                 {
-                    case nameof(Alter):
-                        if (Alter < 0 || Alter > 100) return "Bitte gib dein wahres Alter ein";
-
+                    case (nameof(Alter)):
+                        if (Alter < 0 || Alter > 150) return "Bitte gib dein wahres Alter an.";
                         break;
                     default:
                         break;
@@ -43,6 +56,11 @@ namespace Validation
 
                 return null;
             }
+        }
+
+        public Person()
+        {
+            this.IP = String.Empty;
         }
     }
 }
